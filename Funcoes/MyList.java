590ddf8065d2ae97a862cb5e7e4776634056b370
rafa00el00 <whitestrapes@gmail.com>
2016-09-26 16:03:07 -0300

    Criado o minha lista(MyLista)

---
+++ Funcoes/MyList.java
@@ -0,0 +1,20 @@
+package Funcoes;
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class MyList<E> extends ArrayList<E> {
+
+	public MyList<E> find(Predicate<E> func){
+		MyList<E> lst = new MyList<E>();
+		
+		for(E item : this ){
+			if (func.test(item)){
+				lst.add(item);				
+			}
+		}
+			
+		return lst;
+	}
+	
+	
+}
